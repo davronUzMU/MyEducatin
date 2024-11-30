4341da8cc4069c21e615768158a8c24085ebe24b
Davron <boriboyevdavron9@gmail.com>
2024-11-30 17:20:24 +0500

    React

--- AllDTO/TestDTO/QuestionDTO/QuestionResponseDTO.cs
+++ AllDTO/TestDTO/QuestionDTO/QuestionResponseDTO.cs
@@ -1,7 +1,17 @@
-﻿namespace MyEducatin.AllDTO.TestDTO.QuestionDTO
+﻿using MyEducatin.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyEducatin.AllDTO.TestDTO.QuestionDTO
 {
     public class QuestionResponseDTO
     {
-
+        public int Id { get; set; } // Savol uchun unikal identifikator
+        [Required]
+        public string Text { get; set; } = string.Empty; // Savol matni
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+        public List<Answer> Answers { get; set; } // Ushbu savolga tegishli javoblar
+        public int CorrectAnswerId { get; set; } // To'g'ri javobning id
+        public State State { get; set; }
     }
 }
